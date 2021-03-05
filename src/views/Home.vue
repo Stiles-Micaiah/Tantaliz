3f5089c056b1eeaaf78e16ee703b6e47fd580389
Stiles-Micaiah <syluxprime40@gmail.com>
2021-03-05 07:10:12 -0700

    Adding footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,16 +30,19 @@
 <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
